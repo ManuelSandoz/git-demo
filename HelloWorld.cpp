c7a74f67289f5b6030992bd97420fc343deeb9fd
Alex Rosario <alex.rosarioh@outlook.com>
2021-01-27 15:35:55 -0400

    Make this more politically-correct

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
   cout << "Hello World, my name is Manuel\n";
 
-
-  for(int i = 0; i < 10; ++i){
-    cout<< "Fuck You" << endl;
+  cout << "It's the end of the world as we know it\n";
+  for (int i = 0; i < 10; ++i)
+  {
+    cout << "No" << endl;
   }
   return 0;
 }
