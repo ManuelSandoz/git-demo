f60fe69480a4631085582ea1d91463c6cb676613
Alex Rosario <alex.rosarioh@outlook.com>
2021-01-27 15:27:04 -0400

    please work

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
   cout << "Hello World, my name is Manuel\n";
 
+  cout << "It's the end of the world as we know it\n";
 
-  for(int i = 0; i < 10; ++i){
-    cout<< "Fuck You" << endl;
+  for (int i = 0; i < 10; ++i)
+  {
+    cout << "Fuck You" << endl;
   }
   return 0;
 }
