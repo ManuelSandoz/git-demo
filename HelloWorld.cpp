bda893f676087a20a87cde63ab6c7fab5640a4ae
Manuel Sandoz <manuelsandoz28@gmail.com>
2021-01-27 16:06:59 -0400

    Changed code back to the original to record the branch video

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -3,12 +3,7 @@ using namespace std;
 
 int main()
 {
-  cout << "Hello World, my name is Manuel\n";
+  cout << "Hello World\n";
 
-  cout << "It's the end of the world as we know it\n";
-  for (int i = 0; i < 10; ++i)
-  {
-    cout << "No" << endl;
-  }
   return 0;
 }
