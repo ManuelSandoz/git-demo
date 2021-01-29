6e598ac4df9506ad4dc0d9b5fd7ab22b358a150e
Alex Rosario <alex.rosarioh@outlook.com>
2021-01-28 23:21:16 -0400

    Add loop for counting to 10 and their powers

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -4,6 +4,10 @@ using namespace std;
 int main()
 {
   cout << "Hello there!\n";
-
+  for (int i = 0; i < 10; i++)
+  {
+    cout << i + 1 << endl;
+    cout << (i + 1) * (i + 1) << endl;
+  }
   return 0;
 }
