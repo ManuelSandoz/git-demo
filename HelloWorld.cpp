77c266cab836a88c458d071d48e1ad91fc8ace52
Alex Rosario <alex.rosarioh@outlook.com>
2021-02-18 10:22:08 -0400

    Change a mathematical operation

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -4,15 +4,10 @@ using namespace std;
 int main()
 {
   cout << "Hello there!\n";
-<<<<<<< HEAD
   for (int i = 0; i < 10; i++)
   {
-    cout << i + 1 << endl;
     cout << (i + 1) * (i + 1) << endl;
+    cout << (2*i) * (2*i) << endl;
   }
-=======
-  cout<< "No talk me I angy >:[ \n";
-
->>>>>>> f44917ed2532ac32019543294f8112988b9280e0
   return 0;
 }
